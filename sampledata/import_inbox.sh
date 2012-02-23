e9a4e7698afdab22d6c0c0b13cae5250e1ac1548
Tudor Oprea <tudor@thudspace.net>
2012-02-23 02:14:39 +0100

    data dirs

---
+++ sampledata/import_inbox.sh
@@ -0,0 +1,1 @@
+rake --trace import:dir dir=sampledata/inbox username=test account=default label=INBOX
---
+++ sampledata/import_sentmail.sh
@@ -0,0 +1,1 @@
+rake --trace import:dir dir=sampledata/sent_mail username=test account=default label="Sent Mail"
